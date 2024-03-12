3fc3ce0ca3a3b57c37501cd56946c76183ae2615
Purnesh Patel <purneshp@cybage.com>
2024-03-12 16:02:02 +0530

    Some assembly changes.

--- obj/Debug/net6.0/API_Application.AssemblyInfo.cs
+++ obj/Debug/net6.0/API_Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8663339711872ed0fc32f15d5c584cf3a22747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797ea70130a775bbc14ede2de987e4116c616826")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
